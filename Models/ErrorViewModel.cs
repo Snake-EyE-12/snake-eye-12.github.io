608d0f3a726301bfa7cac7b1a080b403e17a2877
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2024-05-28 21:55:12 -0600

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Multimedia_Portfolio.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
